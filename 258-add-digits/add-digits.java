f45adc8554e1a6ab5e8ffc4f0900d3190700df40
Ishika <ishikahere04@gmail.com>
2026-03-03 16:16:01 +0530

    Time: 1 ms (97.85%) | Memory: 42.6 MB (64.50%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,6 +1,7 @@
 class Solution {
     public int addDigits(int num) {
-       while(num >= 10)
+       if(num < 10) return num;//for single digit
+       while(num >= 10) //to check more than 1 digit
        {
         int sum = 0;
         while(num > 0)
